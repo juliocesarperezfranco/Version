4b6ee5a2a3a35bb6c2d8c558e82f20eaea0acef3
Julio Cesar Pérez Franco <julio.cesar.perez.franco@gmail.com>
2021-02-03 21:56:50 -0500

    Create VersionInfo.h
    Simple Version structure that can be used for almost anything.

---
+++ src/VersionInfo.h
@@ -0,0 +1,12 @@
+#ifndef LIBVERSION_VERSIONINFO_H
+#define LIBVERSION_VERSIONINFO_H
+
+struct VersionInfo
+{
+  unsigned int major;
+  unsigned int minor;
+  unsigned int build;
+  unsigned int patch;
+};
+
+#endif // LIBVERSION_VERSIONINFO_H
